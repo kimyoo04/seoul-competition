01baf8059f7b3232828c3712741b922c85d05fa9
Kim Yoo <kimyoo04eco@naver.com>
2023-04-18 19:34:28 +0900

    style: 쌍따옴표로 교체

--- next/tailwind.config.js
+++ next/tailwind.config.js
@@ -1,47 +1,47 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
-  darkMode: 'class', // next-themes 다크모드 설정
+  content: ["./src/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class", // next-themes 다크모드 설정
   theme: {
     extend: {
       colors: {
-        main_color: '#2F80ED',
-        sub_color: '#96d0f2',
+        main_color: "#2F80ED",
+        sub_color: "#96d0f2",
 
-        gray_1: '#303030',
-        gray_2: '#808080',
-        gray_3: '#BDBDBD',
-        gray_4: '#EEEEEE',
+        gray_1: "#303030",
+        gray_2: "#808080",
+        gray_3: "#BDBDBD",
+        gray_4: "#EEEEEE",
 
-        bg_1: '#FBFBFB',
+        bg_1: "#FBFBFB",
 
-        font_1: '#101010',
+        font_1: "#101010",
 
-        alert_success: '#219653',
-        alert_warning: '#F2C94C',
-        alert_danger: '#EB5757',
-        alert_info: '#2F80ED',
+        alert_success: "#219653",
+        alert_warning: "#F2C94C",
+        alert_danger: "#EB5757",
+        alert_info: "#2F80ED",
       },
       fontFamily: {
-        base: 'IBM Plex Sans KR, system-ui, sans-serif',
+        base: "IBM Plex Sans KR, system-ui, sans-serif",
       },
       fontSize: {
-        h1: '68px',
-        h2: '42px',
-        h3: '26px',
-        h4: '20px',
-        medium: '16px',
-        small: '14px',
+        h1: "68px",
+        h2: "42px",
+        h3: "26px",
+        h4: "20px",
+        medium: "16px",
+        small: "14px",
       },
       spacing: {
         // header: "66px",
         // sidebar: "320px",
       },
       maxWidth: {
-        container: '1080px',
+        container: "1080px",
       },
       minWidth: {
-        sidebar: '320px',
+        sidebar: "320px",
       },
     },
   },
