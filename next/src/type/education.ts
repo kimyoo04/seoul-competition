46fe94a8de0c984fa9ae943b6458b6e2d665f6d0
Kim Yoo <kimyoo04eco@naver.com>
2023-04-20 02:09:52 +0900

    feat: 임시 education data interface 설정

---
+++ next/src/type/education.ts
@@ -0,0 +1,12 @@
+export interface IEducation {
+  id: number;
+  name: string;
+  applyStart: string;
+  applyEnd: string;
+  start: string;
+  end: string;
+  capacity: number;
+  status: string;
+  price: number;
+  link: string;
+}
