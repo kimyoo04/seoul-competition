ddd815675dcc4fd49ff91540c0beab9e4919249d
Kim Yoo <kimyoo04eco@naver.com>
2023-04-20 02:11:50 +0900

    feat: 교육 강좌 card 컴포넌트 제작 (ongoing)

--- next/src/scenes/Education/index.tsx
+++ next/src/scenes/Education/index.tsx
@@ -1,3 +1,19 @@
+import ScrollButton from "@components/ScrollButton";
+import { educationData } from "public/data/educationData";
+import Card from "./Card";
+
 export default function Education() {
-  return <div className="container px-5 ">education</div>;
+  return (
+    <div className="w-screen h-screen px-4 bg-gray_4">
+      {/* 교육 리스트 영역 */}
+      <div className="gap-3 col-center">
+        {educationData.map((data) => (
+          <Card key={data.id} data={data} />
+        ))}
+      </div>
+
+      {/* 최상단 이동 버튼 */}
+      <ScrollButton />
+    </div>
+  );
 }
