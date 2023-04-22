b4ee679560ec918ecb0d16a6c4cdc53684f3fab9
Kim <58503130+kimyoo04@users.noreply.github.com>
2023-04-22 22:20:45 +0900

    feat: postDetail 페이지 구현

--- next/pages/posts/[id].tsx
+++ next/pages/posts/[id].tsx
@@ -1,13 +1,19 @@
 import { useRouter } from "next/router";
 import MainLayout from "@layouts/MainLayout";
+import PostDetail from "@scenes/Posts/postDetail";
 
-export default function BoardDetailPage() {
+export default function PostDetailPage() {
   const router = useRouter();
   const { id } = router.query;
 
-  return (
-    <MainLayout>
-      <h1>Board {id}</h1>
-    </MainLayout>
-  );
+  if (typeof id === "string") {
+    return (
+      <MainLayout>
+        <PostDetail id={id} />
+      </MainLayout>
+    );
+  } else {
+    // 잘못된 id를 받으면 자유게시판 redirect
+    router.push("/posts");
+  }
 }
