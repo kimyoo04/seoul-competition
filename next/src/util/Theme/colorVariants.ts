9c92fd31f86e55ab22e7f9b6812c9b4e90c4fb94
chae-zero <115343657+chae-zero@users.noreply.github.com>
2023-04-23 12:47:13 +0900

    feat: Theme의 colorVariants type 설정

---
+++ next/src/util/Theme/colorVariants.ts
@@ -0,0 +1,8 @@
+export const colorVariants = {
+  maincolor: {
+    textActive: "text-maincolor",
+    textHover: "group-hover:text-maincolor",
+    borderActive: "w-48 bg-maincolor ",
+    borderHover: "group-hover:bg-maincolor",
+  },
+};
