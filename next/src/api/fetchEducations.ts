dd08ee12259da3fd50131cfb683f09c24f1706a1
Kim Yoo <kimyoo04eco@naver.com>
2023-04-24 21:48:41 +0900

    fix: cors 설정 변경, dotenv 적용 후 url 수정, return data 형식 일치시키기

--- next/src/api/fetchEducations.ts
+++ next/src/api/fetchEducations.ts
@@ -4,7 +4,7 @@ import axios from "@api/axiosInstance";
 export const fetchEducations = createAsyncThunk(
   "educations/fetcheducations",
   async (page: number) => {
-    const response = await axios.get(`/api/educations?page=${page}`);
-    return response.data;
+    const response = await axios.get(`/educations?page=${page}`);
+    return response.data.data;
   }
 );
--- next/src/api/fetchPostDetail.ts
+++ next/src/api/fetchPostDetail.ts
@@ -4,7 +4,7 @@ import axios from "@api/axiosInstance";
 export const fetchPostDetail = createAsyncThunk(
   "postDetail/fetchpostDetail",
   async (id: string) => {
-    const response = await axios.get(`/api/posts/${id}`);
-    return response.data;
+    const response = await axios.get(`/posts/${id}`);
+    return response.data.data;
   }
 );
