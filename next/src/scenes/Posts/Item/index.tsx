1bb8954661cadafbd7f140bf924c63725cd87481
chae-zero <115343657+chae-zero@users.noreply.github.com>
2023-04-24 21:00:52 +0900

    feat: scenes의 posts, postDetail UI (더미데이터 사용)

--- next/src/scenes/Posts/Item/index.tsx
+++ next/src/scenes/Posts/Item/index.tsx
@@ -1,27 +1,55 @@
-import { IPost } from "@type/post";
+import { IPost } from "@type/posts";
+import Link from "next/link";
 
 export default function Item({ post }: { post: IPost }) {
   return (
-    <div>
-      <div>{post.id}</div>
+    <Link
+      href={`/posts/${post.id}`}
+      className="flex flex-col w-full gap-4 p-4 bg-white shadow-md rounded-2xl"
+    >
+      {/* 작성자명과 작성일자 */}
+      <div className="flex items-center justify-between mb-2">
+        <div className="text-sm text-gray-500">{post.nickname}</div>
+      </div>
 
-      {/* 닉네임 */}
-      <div>{post.nickname}</div>
+      <div>
+        <div className="font-bold text-lg mb-2">{post.title}</div>
+        <div className="text-gray-800">{post.content}</div>
+      </div>
 
-      {/* 글 제목 */}
-      <div>{post.title}</div>
-
-      {/* 일부 내용 */}
-      <div>{post.content}</div>
-
-      {/* 업로드 날짜 */}
-      <div>{post.createdAt}</div>
-
-      {/* 조회 수 */}
-      <div>{post.count}</div>
-
-      {/* 댓글 수 */}
-      <div>{post.comment_num}</div>
-    </div>
+      {/* 조회와 댓글 */}
+      <div className="flex justify-between mt-4">
+        <div className="text-sm text-gray-500">
+          조회 {post.hits} · 댓글 {post.comments_num}
+        </div>
+        <div className="text-sm text-gray-500">
+          {post.createdAt.slice(0, 10)}
+        </div>
+      </div>
+    </Link>
   );
 }
+
+// 큰 단위로 끊어서 주석 달기
+
+// <div>
+//   <div>{post.id}</div>
+
+//   {/* 닉네임 */}
+//   <div>{post.nickname}</div>
+
+//   {/* 글 제목 */}
+//   <div>{post.title}</div>
+
+//   {/* 일부 내용 */}
+//   <div>{post.content}</div>
+
+//   {/* 업로드 날짜 */}
+//   <div>{post.createdAt}</div>
+
+//   {/* 조회 수 */}
+//   <div>{post.hits}</div>
+
+//   {/* 댓글 수 */}
+//   <div>{post.comments_num}</div>
+// </div>
--- next/src/scenes/Posts/postDetail/index.tsx
+++ next/src/scenes/Posts/postDetail/index.tsx
@@ -6,50 +6,105 @@ import ScrollButton from "@components/ScrollButton";
 import { useAppDispatch, useAppSelector } from "@toolkit/hook";
 import { fetchPostDetail } from "@api/fetchPostDetail";
 
-export default function PostDetail({ id }: { id: string }) {
-  const dispatch = useAppDispatch();
-  const { post, error, status } = useAppSelector((state) => state.postDetail);
+import { postDetail1 } from "public/data/postData";
 
-  // 최소 게시글 디테일 API 요청
-  useEffect(() => {
-    dispatch(fetchPostDetail(id));
-  }, []);
+function timeSince(date: string) {
+  const seconds = Math.floor((new Date().getTime() - new Date(date).getTime()) / 1000);
+
+  let interval = seconds / 31536000;
+
+  if (interval > 1) {
+    return Math.floor(interval) + "년 전";
+  }
+  interval = seconds / 2592000;
+  if (interval > 1) {
+    return Math.floor(interval) + "개월 전";
+  }
+  interval = seconds / 86400;
+  if (interval > 1) {
+    return Math.floor(interval) + "일 전";
+  }
+  interval = seconds / 3600;
+  if (interval > 1) {
+    return Math.floor(interval) + "시간 전";
+  }
+  interval = seconds / 60;
+  if (interval > 1) {
+    return Math.floor(interval) + "분 전";
+  }
+  return Math.floor(seconds) + "초 전";
+}
+
+export default function PostDetail({ id }: { id: string }) {
+  // const dispatch = useAppDispatch();
+  // const { post, error, status } = useAppSelector((state) => state.postDetail);
+
+  // // 최소 게시글 디테일 API 요청
+  // useEffect(() => {
+  //   dispatch(fetchPostDetail(id));
+  // }, []);
 
   return (
-    <div className="w-screen h-screen px-4 bg-gray_4">
-      {status == "loading" ? (
-        // loading 화면 출력
-        <Loading />
-      ) : status == "failed" ? (
-        // failed 화면 출력
-        error && <p className="text-alert_danger">{error}</p>
-      ) : (
-        // fetch 성공 화면 출력
-        <div>
-          {/* 게시글 내용 영역 */}
-          <div>{post.id}</div>
-          <div>{post.nickname}</div>
-          <div>{post.title}</div>
-          <div>{post.content}</div>
-          <div>{post.createdAt}</div>
-          <div>{post.count}</div>
-
-          {/* 댓글 영역 */}
-          <div>
-            {post.comments.map((comment) => (
-              <div>
-                <div>{comment.id}</div>
-                <div>{comment.post_id}</div>
-                <div>{comment.nickname}</div>
-                <div>{comment.content}</div>
-              </div>
-            ))}
+    <div className="px-4 rounded-2xl shadow-md bg-gray-100">
+      <div className="max-w-screen-lg mx-auto my-8">
+        <div className="bg-white rounded-2xl shadow-lg p-8">
+          <h2 className="text-2xl font-bold mb-4">{postDetail1.title}</h2>
+          <div className="text-gray-500 mb-4">
+            <span>작성자: {postDetail1.nickname}</span>
+            <span className="mx-2">|</span>
+            <span>작성일: {postDetail1.createdAt.slice(0, 10)}</span>
+            <span className="mx-2">|</span>
+            <span>조회수: {postDetail1.hits}</span>
           </div>
+          <div className="text-gray-800 mb-8">{postDetail1.content}</div>
+          <h3 className="text-lg font-semibold mb-4">댓글</h3>
+          {postDetail1.comments.map((comment) => (
+            <div key={comment.id} className="bg-gray-100 rounded-lg p-4 mb-4">
+              <div className="text-gray-500 mb-2">
+                <span>{comment.nickname}</span>
+                <span className="mx-2">|</span>
+                <span>{comment.createdAt.slice(0, 10)}</span>
+                <span className="mx-2">|</span>
+                <span>{timeSince(comment.createdAt)}</span>
+              </div>
+              <div className="text-gray-800">{comment.content}</div>
+            </div>
+          ))}
         </div>
-      )}
-
-      {/* 최상단 이동 버튼 */}
+      </div>
       <ScrollButton />
     </div>
   );
 }
+
+// {status == "loading" ? (
+//   // loading 화면 출력
+//   <Loading />
+// ) : status == "failed" ? (
+//   // failed 화면 출력
+//   error && <p className="text-alert_danger">{error}</p>
+// ) : (
+//   // fetch 성공 화면 출력
+//   <div>
+//     {/* 게시글 내용 영역 */}
+//     <div>{post.id}</div>
+//     <div>{post.nickname}</div>
+//     <div>{post.title}</div>
+//     <div>{post.content}</div>
+//     <div>{post.createdAt}</div>
+//     <div>{post.hits}</div>
+
+//     {/* 댓글 영역 */}
+//     <div>
+//       {post.comments.map((comment) => (
+//         <div>
+//           <div>{comment.id}</div>
+//           <div>{comment.post_id}</div>
+//           <div>{comment.nickname}</div>
+//           <div>{comment.content}</div>
+//           <div>{comment.createdAt}</div>
+//         </div>
+//       ))}
+//     </div>
+//   </div>
+// )}
