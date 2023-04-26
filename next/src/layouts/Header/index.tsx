e99382cb299ed3937c7a157157f0d6c7737f36c8
chae-zero <115343657+chae-zero@users.noreply.github.com>
2023-04-26 16:42:48 +0900

    css:  Header border-b -> shadow- sm 으로 대체

--- next/src/layouts/Header/index.tsx
+++ next/src/layouts/Header/index.tsx
@@ -4,7 +4,7 @@ import SideToggle from "./HeaderItem/SideToggle";
 export default function Header() {
   return (
     <>
-      <div className="fixed top-0 z-40 w-full h-16 border-b bg-white">
+      <div className="fixed top-0 z-40 w-full h-16 shadow-sm bg-white">
         <header className="text-gray-600 body-font">
           <div className="container flex flex-wrap items-center justify-between gap-4 p-4 mx-auto">
             <Logo />
