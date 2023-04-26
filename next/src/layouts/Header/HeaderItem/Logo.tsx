b61a5478f45ea7296223ccee936a3db90016081e
chae-zero <115343657+chae-zero@users.noreply.github.com>
2023-04-26 12:06:49 +0900

    css: 로고 불필요한 css 코드 제거

--- next/src/layouts/Header/HeaderItem/Logo.tsx
+++ next/src/layouts/Header/HeaderItem/Logo.tsx
@@ -3,20 +3,15 @@ import { motion } from "framer-motion";
 
 export default function Logo() {
   return (
-    <Link href={""} className="relative h-[32px]">
-      <div className="absolute left-0 top-2">
-        <div className="top-[8px] h-[1px] w-full bg-light_gray_1 dark:bg-night_gray_1" />
-        <div className="w-[72px] z-20 h-4"></div>
-        <div className="bottom-[8px] h-[1px] w-full bg-light_gray_1 dark:bg-night_gray_1" />
-      </div>
+    <Link href={"/"} className="relative h-[32px]">
       <motion.div
         whileTap={{
           scale: 0.9,
         }}
         className="absolute w-[72px] z-20"
       >
-        <span className="text-2xl font-bold text-main_color dark:text-main_color">
-        Senior+
+        <span className="text-2xl font-semibold text-main_color dark:text-main_color">
+          Senior+
         </span>
       </motion.div>
     </Link>
