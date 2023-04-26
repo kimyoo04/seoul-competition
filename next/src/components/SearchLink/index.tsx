99fb05da06f33c615e41d79e7b9f2d21583e25cf
Kim Yoo <kimyoo04eco@naver.com>
2023-04-26 23:00:08 +0900

    feat: 헤더에 넣기 위한 SearchLink 대폭 수정

--- next/src/components/SearchLink/index.tsx
+++ next/src/components/SearchLink/index.tsx
@@ -1,35 +1,36 @@
 import Link from "next/link";
-import { TSearchCategory } from "@type/search";
+import { useRouter } from "next/router";
+
 import { useAppDispatch } from "@toolkit/hook";
 import { searchActions } from "@features/search/searchSlice";
 
-export default function SearchLink({
-  category,
-}: {
-  category: TSearchCategory;
-}) {
+import ButtonWrapper from "@components/Animation/ButtonWrapper";
+
+export default function SearchLink() {
   const dispatch = useAppDispatch();
+  const router = useRouter();
+  const currentPath = router.pathname.substring(1);
 
   return (
-    <Link
-      href={"/search"}
-      onClick={() =>
-        // searchCategory state 변경
-        dispatch(searchActions.chooseCategory({ searchCategory: category }))
-      }
-    >
-      <div className="relative w-full group">
-        {/* Search Bar 영역 */}
-        <div className="absolute w-full h-8 pl-4 transition-all bg-white border rounded-full group-hover:border-main_color"></div>
-
+    currentPath !== "search" && (
+      <Link
+        href={"/search"}
+        onClick={() =>
+          // searchCategory state 변경
+          dispatch(
+            searchActions.chooseCategory({
+              searchCategory: currentPath,
+            })
+          )
+        }
+      >
         {/* Search Bar 아이콘 */}
-        <button className="absolute text-2xl font-bold right-4 col-center ">
-          <i className="transition-all ri-search-line group-hover:text-main_color"></i>
-        </button>
-
-        {/* 더미 div 태그 */}
-        <div className="w-full h-8 dummy"> </div>
-      </div>
-    </Link>
+        <ButtonWrapper>
+          <button className="col-center">
+            <i className="ri-search-line text-3xl text-main_color"></i>
+          </button>
+        </ButtonWrapper>
+      </Link>
+    )
   );
 }
