691805e37f7e05537c05cbdf431cb02b76549678
chae-zero <115343657+chae-zero@users.noreply.github.com>
2023-04-26 18:54:58 +0900

    fix: NavLinsks 클릭 시 사이드바 사라지도록 수정

--- next/src/layouts/Header/Sidebar/NavLinks.tsx
+++ next/src/layouts/Header/Sidebar/NavLinks.tsx
@@ -1,10 +1,12 @@
 import NavLink from "./NavLink";
-
 import { motion } from "framer-motion";
 import { staggerContainer } from "@util/variants/container";
+import { sidebarActions } from "@features/sidebar/sidebarSlice";
+import { useAppDispatch } from "@toolkit/hook";
 
 // isClicked를 기준으로 Menu 토글
 export default function NavLinks() {
+  const dispatch = useAppDispatch();
   return (
     <motion.nav
       variants={staggerContainer}
@@ -13,9 +15,15 @@ export default function NavLinks() {
       viewport={{ once: false, amount: 0.25 }}
       className={`w-48 gap-4 col-start sm:mt-auto`}
     >
-      <NavLink linkpath={"/"} name={"Home"} delay={0.1} />
-      <NavLink linkpath={"posts"} name={"Posts"} delay={0.2} />
-      <NavLink linkpath={"educations"} name={"Educations"} delay={0.3} />
+      <div onClick={() => dispatch(sidebarActions.toggleSidebar())}>
+        <NavLink linkpath={""} name={"Home"} delay={0.1} />
+      </div>
+      <div onClick={() => dispatch(sidebarActions.toggleSidebar())}>
+        <NavLink linkpath={"posts"} name={"Posts"} delay={0.2} />
+      </div>
+      <div onClick={() => dispatch(sidebarActions.toggleSidebar())}>
+        <NavLink linkpath={"educations"} name={"Educations"} delay={0.3} />
+      </div>
     </motion.nav>
   );
 }
