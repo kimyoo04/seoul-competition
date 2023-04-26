96ab140d82f64a7e6cbe95e7cfec64c5f597b7a3
chae-zero <115343657+chae-zero@users.noreply.github.com>
2023-04-26 16:35:45 +0900

    css: Logo, Sidetoggle 높이, 너비 맞추기, 폰트 굵기 수정

--- next/src/layouts/Header/HeaderItem/SideToggle.tsx
+++ next/src/layouts/Header/HeaderItem/SideToggle.tsx
@@ -1,16 +1,21 @@
 import React from "react";
 import { useAppDispatch } from "@toolkit/hook";
 import { sidebarActions } from "@features/sidebar/sidebarSlice";
+import ButtonWrapper from "@components/Animation/ButtonWrapper";
 
 export default function SideToggle() {
   const dispatch = useAppDispatch();
 
   return (
+    <div className="w-[72px] h-8 col-end">
+      <ButtonWrapper>
     <button
-      className="w-8 h-8 col-center"
+      className="w-6 h-6 col-center rounded-2xl shadow-md"
       onClick={() => dispatch(sidebarActions.toggleSidebar())}
     >
-      <i className="text-xl font-semibold ri-menu-3-line" />
+      <i className="text-xl text-main_color ri-menu-3-line" />
     </button>
+    </ButtonWrapper>
+    </div>
   );
 }
