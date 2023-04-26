fa7fea189416696bd07e44c43ae520a302e3bed9
Kim Yoo <kimyoo04eco@naver.com>
2023-04-26 11:38:13 +0900

    feat: Search 페이지 시작

---
+++ next/pages/search/index.tsx
@@ -0,0 +1,10 @@
+import OnePageLayout from "@layouts/OnePageLayout";
+import Search from "@scenes/Search";
+
+export default function SearchPage() {
+  return (
+    <OnePageLayout>
+      <Search />
+    </OnePageLayout>
+  );
+}
