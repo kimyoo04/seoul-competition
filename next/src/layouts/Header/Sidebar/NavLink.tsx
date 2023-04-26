573eeb97bebeeb96dfabdd09546879f904947e21
chae-zero <115343657+chae-zero@users.noreply.github.com>
2023-04-26 12:09:34 +0900

    refactor: Menu -> Sidebar 폴더명 수정 및 portal 생성

--- next/src/layouts/Header/Sidebar/NavLink.tsx
+++ next/src/layouts/Header/Sidebar/NavLink.tsx
@@ -1,6 +1,4 @@
-import { colorVariants } from "@util/Theme/colorVariants";
 import Link from "next/link";
-import { useRouter } from "next/router";
 import { motion } from "framer-motion";
 import { fadeIn } from "@util/variants";
 
@@ -13,13 +11,12 @@ export default function NavLink({
   name: string;
   delay: number;
 }) {
-  const router = useRouter();
   const link = `/${linkpath}`;
 
   return (
     <motion.div variants={fadeIn("left", "easeInOut", delay, 0.4)}>
       <Link href={link} className="group">
-        <span className={`text-4xl transition-all duration-500`}>{name}</span>
+        <span className={`text-2xl transition-all duration-500`}>{name}</span>
       </Link>
     </motion.div>
   );
---
+++ next/src/layouts/Header/Sidebar/index.tsx
@@ -0,0 +1,37 @@
+import { AnimatePresence, motion } from "framer-motion";
+import NavLinks from "./NavLinks";
+import { useAppDispatch, useAppSelector } from "@toolkit/hook";
+import { sidebarActions } from "@features/sidebar/sidebarSlice";
+import Portal from "@components/Portal";
+
+const SideBar = () => {
+  const dispatch = useAppDispatch();
+  const isSidebar = useAppSelector((state) => state.sidebar.isSidebar);
+
+  return (
+    <AnimatePresence>
+      {/* 메뉴 컴포넌트가 isClicked에 토글 됨 */}
+      {isSidebar && (
+        <motion.div
+          initial={{ x: 300 }}
+          animate={{ x: 0 }}
+          exit={{ x: 300 }}
+          transition={{ duration: 0.2 }}
+          className="absolute right-0 z-40 w-64 h-screen max-w-md p-4 bg-blue-500 shadow-lg"
+          onClick={() => dispatch(sidebarActions.toggleSidebar())}
+        >
+          {/* 닫기 버튼 */}
+          <button className="text-gray-400 hover:text-gray-500">
+            <i className="text-3xl ri-close-line" />
+          </button>
+
+          {/* Modal의 Children = NavLinks */}
+          <NavLinks />
+        </motion.div>
+      )}
+    </AnimatePresence>
+  );
+};
+
+export default Portal(SideBar);
+
