07c7a8d8c9bc44c57a89933d3d90f85f55424e76
Kim Yoo <kimyoo04eco@naver.com>
2023-04-26 14:06:55 +0900

    feat: Search 컴포넌트에 stagger animation 구현

--- next/src/scenes/Search/index.tsx
+++ next/src/scenes/Search/index.tsx
@@ -1,7 +1,38 @@
+import { useEffect } from "react";
+
+import { animate, motion, stagger } from "framer-motion";
+
+import SearchBar from "@components/SearchBar";
+import SearchHistory from "./SearchHistory";
+import SearchRanking from "./SearchRanking";
+
 export default function Search() {
+  // stagger 정의
+  const staggerSearchItems = stagger(0.3, { startDelay: 0.3 });
+
+  // 애니메이션 실행
+  useEffect(() => {
+    animate(
+      "li",
+      { opacity: [0, 1], y: [100, 0] },
+      { duration: 0.3, delay: staggerSearchItems }
+    );
+  }, []);
+
   return (
-    <div className="w-full h-full">
-      <div>Search</div>
+    <div className="grid w-full h-full gap-4 grid-col-1">
+      {/* 텍스트 입력 검색 영역 */}
+      <li>
+        <SearchBar />
+      </li>
+
+      {/* 검색 기록 */}
+      <li>
+        <SearchHistory />
+      </li>
+      <li>
+        <SearchRanking />
+      </li>
     </div>
   );
 }
