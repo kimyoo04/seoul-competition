1ecd69769ff7febab97481aa3a6544089913666e
chae-zero <115343657+chae-zero@users.noreply.github.com>
2023-04-28 03:22:00 +0900

    feat: 헤더에 SearchLink 컴포넌트 추가

--- src/layouts/Header/index.tsx
+++ src/layouts/Header/index.tsx
@@ -1,13 +1,19 @@
 import Logo from "./HeaderItem/Logo";
 import SideToggle from "./HeaderItem/SideToggle";
+import SearchLink from "./HeaderItem/SearchLink";
 
 export default function Header() {
   return (
-    <header className="fixed top-0 z-40 w-full bg-white shadow-sm">
-      <div className="container flex items-center justify-between h-16 gap-4 px-4 mx-auto">
-        <Logo />
-        <SideToggle />
-      </div>
-    </header>
+    <>
+      <header className="fixed top-0 z-40 w-full bg-white shadow-sm">
+        <div className="container flex items-center justify-between h-16 gap-4 px-4 mx-auto">
+          <Logo />
+          <div className="flex gap-4">
+            <SearchLink />
+            <SideToggle />
+          </div>
+        </div>
+      </header>
+    </>
   );
 }
