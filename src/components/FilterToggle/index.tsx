bb23412f16d40de2ffd949bc0b9107d4ca2f0d20
chae-zero <115343657+chae-zero@users.noreply.github.com>
2023-04-29 03:21:18 +0900

    css: FilterToggle 클릭 가능한 영역 확장

--- src/components/FilterToggle/index.tsx
+++ src/components/FilterToggle/index.tsx
@@ -8,16 +8,17 @@ export default function FilterToggle() {
   const dispatch = useAppDispatch();
 
   return (
-    <motion.div
+    <motion.button
       className="col-start fixed -right-4 top-40 h-40 w-16 rounded-l-2xl bg-main_color px-3"
       whileHover={{ x: -15 }}
+      onClick={() => dispatch(sidebarActions.toggleSidebar())}
     >
-      <button onClick={() => dispatch(sidebarActions.toggleSidebar())}>
+      <button>
         <i className="ri-equalizer-line text-2xl text-font_white" />
       </button>
       <span className="ml-[2.5px] w-6 text-xl font-bold text-font_white">
         골라보기
       </span>
-    </motion.div>
+    </motion.button>
   );
 }
