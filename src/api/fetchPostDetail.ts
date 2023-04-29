9d90ed8a37e3f52d767e4883f7eab02bf2a49068
chae-zero <115343657+chae-zero@users.noreply.github.com>
2023-04-29 21:02:52 +0900

    fix: fetchPosts 파일 createAsyncThunk 제거

--- src/api/fetchPostDetail.ts
+++ src/api/fetchPostDetail.ts
@@ -1,10 +1,6 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "@api/axiosInstance";
 
-export const fetchPostDetail = createAsyncThunk(
-  "postDetail/fetchpostDetail",
-  async (id: string) => {
-    const response = await axios.get(`/posts/${id}`);
-    return response.data.data;
-  }
-);
+export const fetchPostDetail = async (id: string) => {
+  const response = await axios.get(`/posts/${id}`);
+  return response.data;
+};
