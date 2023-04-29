3e21c15b00fdd06f1a5b4c776e3bb9d1a8f96b3f
Kim <58503130+kimyoo04@users.noreply.github.com>
2023-04-29 16:39:28 +0900

    css: ChooseCategory 버튼 디자인 수정

--- src/scenes/Search/ChooseCategory/ChooseButton.tsx
+++ src/scenes/Search/ChooseCategory/ChooseButton.tsx
@@ -1,26 +1,29 @@
 import { searchActions } from "@features/search/searchSlice";
 import { useAppDispatch, useAppSelector } from "@toolkit/hook";
-import { TSearchCategory } from "@type/search";
+import { TSearchCategory, TSearchCategoryKor } from "@type/search";
 import classNames from "classnames";
 
 export default function ChooseButton({
   children,
+  categoryEng,
 }: {
-  children: TSearchCategory;
+  children: TSearchCategoryKor;
+  categoryEng: TSearchCategory;
 }) {
   const dispatch = useAppDispatch();
   const searchCategory = useAppSelector((state) => state.search.category);
 
   // 일치하는 카테고리 css 활성화
   const activeCategory = classNames({
-    "text-main_color": searchCategory === children,
+    "text-white font-bold bg-main_color border-main_color shadow-md shadow-main_color/20":
+      searchCategory === categoryEng,
   });
 
   return (
     <button
-      className={` ${activeCategory}`}
+      className={`rounded-full border px-3 py-1 transition-all  ${activeCategory}`}
       onClick={() =>
-        dispatch(searchActions.chooseCategory({ searchCategory: children }))
+        dispatch(searchActions.chooseCategory({ searchCategory: categoryEng }))
       }
     >
       {children}
