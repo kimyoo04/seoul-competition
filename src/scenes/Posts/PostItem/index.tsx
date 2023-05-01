eae9f4b745ff4baef7a6f9bd669426c8f6550daa
chae-zero <115343657+chae-zero@users.noreply.github.com>
2023-05-01 18:41:17 +0900

    fix: util의 dateTime 함수를 활용해 코드 간소화

--- src/scenes/Posts/PostItem/index.tsx
+++ src/scenes/Posts/PostItem/index.tsx
@@ -1,43 +1,8 @@
 import { IPostData } from "@type/posts";
+import { timeSince } from "@util/dateTime";
 import Link from "next/link";
 
 export default function PostItem({ post }: { post: IPostData }) {
-  // Intl API 활용
-  function timeSince(date: string) {
-    // 9시간 더해주기 (UTC - 한국 시간)
-    const koreaTimezoneOffset = 9 * 60;
-
-    // 현재 시각 - 댓글이 쓰인 시각 (초 단위)
-    const seconds = Math.floor(
-      (new Date().getTime() -
-        new Date(date).getTime() -
-        koreaTimezoneOffset * 60 * 1000) /
-        1000
-    );
-
-    // RelativeTimeFormat(): "long" 스타일로 상대적 시간을 표현해줌
-    const rtf = new Intl.RelativeTimeFormat("ko", {
-      style: "long",
-    });
-
-    // 시간 경과 정도에 따라 상대적 시간 표기
-    if (seconds > 604800) {
-      const weeks = Math.floor(seconds / 604800);
-      return rtf.format(-weeks, "week");
-    } else if (seconds > 86400) {
-      const days = Math.floor(seconds / 86400);
-      return rtf.format(-days, "day");
-    } else if (seconds > 3600) {
-      const hours = Math.floor(seconds / 3600);
-      return rtf.format(-hours, "hour");
-    } else if (seconds > 60) {
-      const minutes = Math.floor(seconds / 60);
-      return rtf.format(-minutes, "minute");
-    } else {
-      return rtf.format(-seconds, "second");
-    }
-  }
-
   return (
     // postDetail로 링크
     <Link
