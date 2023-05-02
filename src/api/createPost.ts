e5ca152c438adff787e213c098fca2c834409dcd
Kim Yoo <kimyoo04eco@naver.com>
2023-05-03 02:05:09 +0900

    fix: creeatPost axios POST 요청 수정

--- src/api/createPost.ts
+++ src/api/createPost.ts
@@ -3,10 +3,9 @@ import { IPostForm } from "@type/postForm";
 
 export const createPost = async (newPost: IPostForm) => {
   try {
-    const response = await axios.post(`/posts/new`, newPost);
-    return response.data;
+    const response = await axios.post(`/posts`, newPost);
+    return response; // 성공하면 반환 데이터 없음
   } catch (error) {
-    console.error(error);
-    return null;
+    return error;
   }
 };
