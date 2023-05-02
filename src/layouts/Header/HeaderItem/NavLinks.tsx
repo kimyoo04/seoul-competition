f528e7310e75406b8e9abb6f60812f955bb5e3e0
Kim Yoo <kimyoo04eco@naver.com>
2023-05-02 11:33:22 +0900

    refactor: Header html, css 코드 정리

--- src/layouts/Header/HeaderItem/NavLinks.tsx
+++ src/layouts/Header/HeaderItem/NavLinks.tsx
@@ -4,25 +4,17 @@ import ButtonWrapper from "@components/Animation/ButtonWrapper";
 // isClicked를 기준으로 Menu 토글
 export default function NavLinks() {
   return (
-    <>
-      <div className="pt-[6px] w-[92px] col-center">
-        <Link href={"/educations"}>
-          <ButtonWrapper>
-            <span className=" font-bold text-main_color dark:text-main_color">
-              교육 정보
-            </span>
-          </ButtonWrapper>
-        </Link>
-      </div>
-      <div className="pt-[6px] w-[92px] col-center">
-        <Link href={"/posts"}>
-          <ButtonWrapper>
-            <span className="font-bold text-main_color dark:text-main_color">
-              자유게시판
-            </span>
-          </ButtonWrapper>
-        </Link>
-      </div>
-    </>
+    <div className="flex gap-6 text-xl font-bold">
+      <Link href={"/educations"} className="col-center w-[92px] pt-[6px]">
+        <ButtonWrapper>
+          <span className="text-main_color">교육 정보</span>
+        </ButtonWrapper>
+      </Link>
+      <Link href={"/posts"} className="col-center w-[92px] pt-[6px]">
+        <ButtonWrapper>
+          <span className="text-main_color">자유게시판</span>
+        </ButtonWrapper>
+      </Link>
+    </div>
   );
 }
