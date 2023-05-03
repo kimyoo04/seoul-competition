47092e585346d30230597d86825a7e7c3860a88e
chae-zero <115343657+chae-zero@users.noreply.github.com>
2023-05-04 03:08:31 +0900

    feat: 게시글 수정 버튼에 Link 추가

--- src/scenes/Posts/postDetail/PostDetailItem/UpDelButtons.tsx
+++ src/scenes/Posts/postDetail/PostDetailItem/UpDelButtons.tsx
@@ -1,9 +1,10 @@
+import { IPostDetail } from "@type/postDetail";
 import Link from "next/link";
 
-export default function UpDelButtons() {
+export default function UpDelButtons({ id }: { id: number }) {
   return (
     <div className="text-sm">
-      <Link href="/posts/modify">
+      <Link href={`/posts/modify/${id}`}>
         <button className="m-0.5 rounded-lg bg-gray_2 px-2 py-1 text-font_white">
           수정
         </button>
