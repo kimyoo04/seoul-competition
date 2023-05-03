0d8ae56451f324c7a969e6b59e0dbcc61da401f3
chae-zero <115343657+chae-zero@users.noreply.github.com>
2023-05-03 22:04:48 +0900

    refactor: 게시글, 댓글용 수정/삭제 버튼 컴포넌트 분리 (UpdelButtons, CommentUpdelButtons)

--- src/scenes/Posts/postDetail/PostDetailItem/CommentUpDelButtons.tsx
+++ src/scenes/Posts/postDetail/PostDetailItem/CommentUpDelButtons.tsx
@@ -1,13 +1,13 @@
 import Link from "next/link";
 
-export default function UpDelButtons() {
+export default function CommentUpDelButtons() {
   return (
     <div className="text-sm">
-      <Link href="/posts/modify">
+      {/* <Link href="/commentsy"> */}
         <button className="m-0.5 rounded-lg bg-gray_2 px-2 py-1 text-font_white">
           수정
         </button>
-      </Link>
+      {/* </Link> */}
       <button className="m-0.5 rounded-lg bg-alert_danger px-2 py-1 text-font_white">
         삭제
       </button>
