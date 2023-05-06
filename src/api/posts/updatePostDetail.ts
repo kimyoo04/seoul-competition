8ab9e83a3a37fb55231e12b40592cecdb582fcb6
chae-zero <115343657+chae-zero@users.noreply.github.com>
2023-05-06 13:30:37 +0900

    feat:  게시글 Password Check api 및 useUpdatePostPwdMutation 함수 생성

--- src/api/posts/updatePostDetail.ts
+++ src/api/posts/updatePostDetail.ts
@@ -1,12 +1,23 @@
 import axios from "@api/axiosInstance";
+import { alertActions } from "@features/alert/alertSlice";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { IUpdatePostForm } from "@type/posts";
+import { useAppDispatch } from "@toolkit/hook";
+import { IUpdatePostCheck, IUpdatePostForm } from "@type/posts";
 import { useRouter } from "next/router";
 
 // 게시글 Update
 
 export const updatePostDetail = async (updatedPost: IUpdatePostForm) => {
   const response = await axios.put(`/posts/${updatedPost.postId}`, updatedPost);
+  return response;
+};
+
+// 게시글 Update Password Check
+export const updatePostPwd = async (postIdPwd: IUpdatePostCheck) => {
+  const response = await axios.post(
+    `/posts/${postIdPwd.postId}/matchcheck`,
+    postIdPwd.password
+  );
   return response;
 };
 
@@ -25,6 +36,36 @@ export const useUpdateMutation = () => {
     },
     onError: (err) => {
       console.error(err);
+    },
+    onSettled: () => {
+      console.log("완료");
+    },
+  });
+};
+
+// useUpdateMutation
+export const useUpdatePostPwdMutation = () => {
+  const router = useRouter();
+
+  const dispatch = useAppDispatch();
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: updatePostPwd,
+    onSuccess: async (_, variables) => {
+      await queryClient.invalidateQueries({ queryKey: ["checkedPost"] });
+
+      router.push(`/posts/modify/${variables.postId}`);
+    },
+    onError: (err) => {
+      console.error(err);
+
+      dispatch(
+        alertActions.alert({
+          alertType: "Warning",
+          content: "비밀번호가 일치하지 않습니다. 다시 입력해주세요.",
+        })
+      );
     },
     onSettled: () => {
       console.log("완료");
