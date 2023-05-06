b1eb6929f7923c3d0ecba382ae1184b0c006fbae
Kim Yoo <kimyoo04eco@naver.com>
2023-05-07 00:41:23 +0900

    feat: DetailPageLayout에 Alert 컴포넌트 추가

--- src/layouts/DetailPageLayout.tsx
+++ src/layouts/DetailPageLayout.tsx
@@ -1,14 +1,21 @@
 import { motion } from "framer-motion";
 import Header from "./Header";
+import Alert from "@components/Alert";
+import { useAppSelector } from "@toolkit/hook";
 
 export default function DetailPageLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const isAlert = useAppSelector((state) => state.alert.isAlert);
   return (
     <>
       <Header />
+
+      {/* Alert */}
+      {isAlert && <Alert />}
+
       <motion.div
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
