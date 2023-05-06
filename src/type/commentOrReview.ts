a65ae96ccd8db3a72fbddbb3d79f1909115bb20c
Kim Yoo <kimyoo04eco@naver.com>
2023-05-06 23:45:29 +0900

    feat: comment와 review의 타입을 동시에 관리하는 파일 CommentOrReview.ts 생성

---
+++ src/type/commentOrReview.ts
@@ -0,0 +1,64 @@
+export type TId = number;
+export type TPostId = string | number;
+export type TEducationId = string | number;
+export type TNickname = string;
+export type TPassword = string;
+export type TContent = string;
+export type TCreatedAt = string;
+
+// 댓글과 리뷰 유니온 타입
+export type ICommentOrReview = IComment | IReview;
+
+// 댓글의 read 요청 타입
+export interface IComment extends ICommentOrReviewForm {
+  id: TId;
+  postId: TPostId;
+  createdAt: TCreatedAt;
+}
+
+// 리뷰의 read 요청 타입
+export interface IReview extends ICommentOrReviewForm {
+  id: TId;
+  educationId: TEducationId;
+  createdAt: TCreatedAt;
+}
+
+// 댓글과 리뷰의 create 요청 유니온 타입
+export type ICreateCommentOrReview = ICreateComment | ICreateReview;
+
+// 댓글의 create 요청 유니온 타입
+export interface ICreateComment extends ICommentOrReviewForm {
+  postId: TPostId;
+}
+
+// 리뷰의 create 요청 유니온 타입
+export interface ICreateReview extends ICommentOrReviewForm {
+  educationId: TEducationId;
+}
+
+// 댓글과 리뷰의 폼데이터
+export interface ICommentOrReviewForm {
+  nickname: TNickname;
+  password: TPassword;
+  content: TContent;
+}
+
+// 댓글과 리뷰의 update 요청 유니온 타입
+export interface IUpdateCommentOrReview {
+  id: TId;
+  nickname?: TNickname;
+  password?: TPassword;
+  content?: TContent;
+}
+
+// 댓글과 리뷰의 delete 요청 유니온 타입
+export interface IDeleteCommentOrReview {
+  id: TId;
+  password: TPassword;
+}
+
+// 댓글과 리뷰의 matchCheck 요청 유니온 타입
+export interface IMatchCheckCommentOrReview {
+  id: TId;
+  password: TPassword;
+}
