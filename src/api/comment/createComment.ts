e2b6e0acda82c7b0571cd0aecadfb1b714394f31
chae-zero <115343657+chae-zero@users.noreply.github.com>
2023-05-09 19:08:25 +0900

    feat: createReview api 생성

--- src/api/comment/createComment.ts
+++ src/api/comment/createComment.ts
@@ -1,9 +1,20 @@
 import axios from "@api/axiosInstance";
-import { ICreateComment } from "@type/commentOrReview";
+import { ICreateComment, ICreateReview } from "@type/commentOrReview";
 
+// 자유 게시판 댓글
 export const createComment = async (data: ICreateComment) => {
   try {
     await axios.post(`/comment`, data);
+    return true;
+  } catch (err) {
+    return false;
+  }
+};
+
+// 교육 게시판 리뷰
+export const createReview = async (data: ICreateReview) => {
+  try {
+    await axios.post(`/review`, data);
 
     return true;
   } catch (err) {
