1e4b56058133764dff5cf2b3bd86ff698e1afc05
Kim Yoo <kimyoo04eco@naver.com>
2023-05-09 13:22:03 +0900

    fix: 초기화 버튼 클릭 시 startDate와 endDate의 상태 업데이트 추가

--- src/components/Sidebar/CalendarFilter/index.tsx
+++ src/components/Sidebar/CalendarFilter/index.tsx
@@ -4,18 +4,29 @@ import ko from "date-fns/locale/ko";
 import "react-datepicker/dist/react-datepicker.css";
 
 import { filterActions } from "@features/filter/filterSlice";
-import { useAppDispatch } from "@toolkit/hook";
-import { useState } from "react";
+import { useAppDispatch, useAppSelector } from "@toolkit/hook";
+import { useEffect, useState } from "react";
 import { getBarDate } from "@util/dateTime";
 
 export default function CalendarFilter() {
   const dispatch = useAppDispatch();
+  const { startDate: startDateStr, endDate: endDateStr } = useAppSelector(
+    (state) => state.filter
+  );
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
 
+  // 초기화 버튼을 눌렀을 때 날짜를 초기화
+  useEffect(() => {
+    if (startDateStr === "" && endDateStr === "") {
+      setStartDate(new Date());
+      setEndDate(new Date());
+    }
+  }, [startDateStr, endDateStr]);
+
   return (
     <div className="col-center z-0 w-full ">
-      <span className="mb-2 font-bold text-font_white">기간 탐색</span>
+      <span className="mb-2 font-medium text-font_white">기간 탐색</span>
 
       <div className="row-start w-full ">
         <div className="col-center w-full pr-1">
@@ -38,10 +49,12 @@ export default function CalendarFilter() {
             selectsStart
             maxDate={endDate}
             dateFormat="yyyy-MM-dd"
-            className="textfield m-0 h-8 w-full text-center font-bold shadow-none"
+            className="textfield m-0 h-8 w-full text-center font-medium shadow-none"
           />
         </div>
-        <span className="undraggable text-xl font-bold text-font_white">-</span>
+        <span className="undraggable text-xl font-medium text-font_white">
+          -
+        </span>
         <div className="col-center w-full pl-1">
           <DatePicker
             locale={ko}
@@ -63,7 +76,7 @@ export default function CalendarFilter() {
             minDate={startDate}
             maxDate={new Date()}
             dateFormat="yyyy-MM-dd"
-            className="textfield m-0 h-8 w-full text-center font-bold shadow-none"
+            className="textfield m-0 h-8 w-full text-center font-medium shadow-none"
           />
         </div>
       </div>
