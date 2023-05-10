cc27d0a39864bbf1553a47d456d378f814601f39
Kim Yoo <kimyoo04eco@naver.com>
2023-05-10 16:25:35 +0900

    style: Logo 컴포넌트를 components 폴더로 이동

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -5,7 +5,7 @@ export default function Logo() {
   return (
     <Link href={"/"}>
       <ButtonWrapper>
-        <span className="pb-[6px] pl-[4px] text-3xl font-bold text-main_color dark:text-main_color">
+        <span className="pb-[6px] pl-[4px] text-3xl font-medium text-main_color dark:text-main_color">
           Senior+
         </span>
       </ButtonWrapper>
