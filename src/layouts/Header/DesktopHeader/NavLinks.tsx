b56c5641e30c0195d475922ea8fb61558ddb1187
Kim Yoo <kimyoo04eco@naver.com>
2023-05-10 16:26:05 +0900

    feat: md 사이즈부터 보이는 DesktopHeader 생성

--- src/layouts/Header/DesktopHeader/NavLinks.tsx
+++ src/layouts/Header/DesktopHeader/NavLinks.tsx
@@ -4,15 +4,20 @@ import ButtonWrapper from "@components/Animation/ButtonWrapper";
 // isClicked를 기준으로 Menu 토글
 export default function NavLinks() {
   return (
-    <div className="flex gap-2 text-xl font-bold md:gap-4">
-      <Link href={"/educations"} className="col-end w-[100px] pt-[6px]">
+    <div className="flex gap-2 text-xl font-medium md:gap-4">
+      <Link href={"/educations"} className="col-end w-[84px] pt-[6px]">
         <ButtonWrapper>
           <span className="text-main_color">교육 정보</span>
         </ButtonWrapper>
       </Link>
-      <Link href={"/posts"} className="col-end w-[100px] pt-[6px]">
+      <Link href={"/posts"} className="col-end w-[98px] pt-[6px]">
         <ButtonWrapper>
           <span className="text-main_color">자유게시판</span>
+        </ButtonWrapper>
+      </Link>
+      <Link href={"/chatbot"} className="col-end w-[40px] pt-[6px]">
+        <ButtonWrapper>
+          <span className="text-main_color">챗봇</span>
         </ButtonWrapper>
       </Link>
     </div>
