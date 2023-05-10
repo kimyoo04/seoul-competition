b313eec66bfca4a808e2ebf5cb1ca3dccf9ea8e2
Kim Yoo <kimyoo04eco@naver.com>
2023-05-10 17:26:08 +0900

    feat: 디테일 페이지에서 뒤로가기 버튼을 헤더에 구현

--- src/layouts/Header/MobileHeader/index.tsx
+++ src/layouts/Header/MobileHeader/index.tsx
@@ -1,4 +1,5 @@
 import { Dispatch, SetStateAction } from "react";
+import { useRouter } from "next/router";
 
 import Logo from "@components/Logo";
 import Menu from "./menu";
@@ -11,13 +12,42 @@ export default function MobileHeader({
   showMenu: boolean;
   setShowMenu: Dispatch<SetStateAction<boolean>>;
 }) {
+  const router = useRouter();
+  const pathsArr = router.asPath.split("/");
+  const isDetailPage = router.query.id ? true : false;
+
   return (
     <>
       {/* 헤더 영역 */}
-      <div className="container relative mx-auto flex h-16 items-center justify-between px-4">
-        <Logo />
-        <MenuToggle setShowMenu={setShowMenu} />
-      </div>
+      {isDetailPage ? (
+        <div className="container relative mx-auto grid h-16 grid-cols-3 justify-between px-4">
+          {/* 디테일 페이지 헤더 */}
+          <button className="flex items-center justify-start">
+            <i className="ri-arrow-left-s-line text-4xl text-main_color"></i>
+            <span className="text-lg font-bold text-main_color">뒤로</span>
+          </button>
+
+          <div className="col-center">
+            <h2 className="text-xl font-medium text-main_color">
+              {pathsArr.includes("educations")
+                ? "교육 정보"
+                : pathsArr.includes("posts")
+                ? "자유게시판"
+                : "챗봇"}
+            </h2>
+          </div>
+
+          <div className="flex items-center justify-end">
+            <MenuToggle setShowMenu={setShowMenu} />
+          </div>
+        </div>
+      ) : (
+        <div className="container relative mx-auto flex h-16 items-center justify-between px-4">
+          {/* 일반 페이지 헤더 */}
+          <Logo />
+          <MenuToggle setShowMenu={setShowMenu} />
+        </div>
+      )}
 
       {/* 링크 영역 */}
       {showMenu && <Menu />}
