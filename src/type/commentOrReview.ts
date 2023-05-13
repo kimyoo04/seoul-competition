df78183a328cb007ea51a1f3e1ae6ef5e4dc0a0f
chae-zero <115343657+chae-zero@users.noreply.github.com>
2023-05-13 22:29:54 +0900

    style: commentOrReview 내 타입명 수정

--- src/type/commentOrReview.ts
+++ src/type/commentOrReview.ts
@@ -10,34 +10,33 @@ export type TCreatedAt = string;
 export type ICommentOrReview = IComment | IReview;
 
 // 댓글의 read 요청 타입
-export interface IComment extends ICommentOrReviewForm {
-  id: TId;
+export interface IComment extends IReadCommentOrReview {
   postId: TPostId;
-  createdAt: TCreatedAt;
 }
-
 // 리뷰의 read 요청 타입
-export interface IReview extends ICommentOrReviewForm {
-  id: TId;
+export interface IReview extends IReadCommentOrReview {
   educationId: TEducationId;
+}
+// 댓글과 리뷰의 공통 키 타입
+export interface IReadCommentOrReview {
+  id: TId;
+  nickname: TNickname;
+  content: TContent;
   createdAt: TCreatedAt;
 }
 
 // 댓글과 리뷰의 create 요청 유니온 타입
 export type ICreateCommentOrReview = ICreateComment | ICreateReview;
-
 // 댓글의 create 요청 유니온 타입
-export interface ICreateComment extends ICommentOrReviewForm {
+export interface ICreateComment extends ICreateCommentOrReviewForm {
   postId: TPostId;
 }
-
 // 리뷰의 create 요청 유니온 타입
-export interface ICreateReview extends ICommentOrReviewForm {
+export interface ICreateReview extends ICreateCommentOrReviewForm {
   educationId: TEducationId;
 }
-
 // 댓글과 리뷰의 폼데이터
-export interface ICommentOrReviewForm {
+export interface ICreateCommentOrReviewForm {
   nickname: TNickname;
   password: TPassword;
   content: TContent;
@@ -46,9 +45,9 @@ export interface ICommentOrReviewForm {
 // 댓글과 리뷰의 update 요청 유니온 타입
 export interface IUpdateCommentOrReview extends IUpdateCommentOrReviewForm {
   id: TId;
-  password?: TPassword;
+  password: TPassword;
 }
-// 댓글과 리뷰의 update 요청 유니온 타입
+// 댓글과 리뷰의 update 요청 폼 유니온 타입
 export interface IUpdateCommentOrReviewForm {
   nickname?: TNickname;
   content?: TContent;
@@ -58,13 +57,11 @@ export interface IUpdateCommentOrReviewForm {
 export interface IDeleteCommentOrReview extends IMatchCheckCommentOrReviewForm {
   id: TId;
 }
-
 // 댓글과 리뷰의 matchCheck 요청 유니온 타입
 export interface IMatchCheckCommentOrReview
   extends IMatchCheckCommentOrReviewForm {
   id: TId;
 }
-
 // 댓글과 리뷰의 matchCheck 요청 Form 유니온 타입
 export interface IMatchCheckCommentOrReviewForm {
   password: TPassword;
