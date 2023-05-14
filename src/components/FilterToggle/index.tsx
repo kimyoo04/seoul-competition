684b23bc1c865f1c94f3572dbedb2bb5172f52eb
Kim <58503130+kimyoo04@users.noreply.github.com>
2023-05-14 21:37:16 +0900

    css: fixed button 3개의 motion 적용 제거 및 hover, transition-all 설정 추가

--- src/components/FilterToggle/index.tsx
+++ src/components/FilterToggle/index.tsx
@@ -2,19 +2,16 @@ import React from "react";
 import { useAppDispatch } from "@toolkit/hook";
 import { sidebarActions } from "@features/sidebar/sidebarSlice";
 
-import { motion } from "framer-motion";
-
 export default function FilterToggle() {
   const dispatch = useAppDispatch();
 
   return (
-    <motion.button
-      className="col-start fixed -right-6 top-24 h-40 w-16 rounded-l-2xl bg-main_color pl-2"
-      whileHover={{ x: -8 }}
+    <button
+      className="col-start fixed bottom-[136px] right-4 h-40 w-10 rounded-full bg-white pl-2 shadow-sm shadow-gray-300 transition-all hover:scale-110"
       onClick={() => dispatch(sidebarActions.toggleSidebar())}
     >
-      <i className="ri-equalizer-line text-2xl text-font_white" />
-      <span className="w-6 text-lg font-bold text-font_white">골라보기</span>
-    </motion.button>
+      <i className="ri-equalizer-line text-2xl text-main_color" />
+      <span className="w-6 text-lg font-bold text-main_color">골라보기</span>
+    </button>
   );
 }
