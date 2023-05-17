d3ab3d62eb4a432e68e77545b256325f5bd5dcf7
chae-zero <115343657+chae-zero@users.noreply.github.com>
2023-05-18 06:28:04 +0900

    style: HomePage 레이아웃 변경 (OnePageLayout -> MainLayout)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
-import OnePageLayout from "@layouts/OnePageLayout";
+import MainLayout from "@layouts/MainLayout";
 import Home from "@scenes/Home";
 
 export default function HomePage() {
   return (
-    <OnePageLayout>
+    <MainLayout>
       <Home />
-    </OnePageLayout>
+    </MainLayout>
   );
 }
