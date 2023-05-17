c9dee7b9c9e72f3f723cf01bf97748ecd9324a7e
Kim Yoo <kimyoo04eco@naver.com>
2023-05-17 18:45:56 +0900

    css: 게시글 작성 링크 버튼 motion 제거

--- src/scenes/Posts/CreateButton/index.tsx
+++ src/scenes/Posts/CreateButton/index.tsx
@@ -1,18 +1,14 @@
 import Link from "next/link";
-import { motion } from "framer-motion";
 
 export default function CreateButton() {
   return (
-    <motion.button
-      whileHover={{ scale: 1.1, transition: { duration: 0.05 } }}
-      whileTap={{
-        scale: 0.8,
-      }}
-      className="row-center fixed bottom-6 left-0 right-0 z-10 mx-auto h-10 w-20 rounded-full bg-main_color shadow-md"
+    <Link
+      href="/posts/new"
+      className="row-center fixed bottom-6 left-0 right-0 z-10 mx-auto h-10 w-20 rounded-full bg-main_color shadow-md transition-all hover:scale-125"
     >
-      <Link href="/posts/new">
+      <button>
         <i className="ri-pencil-line text-2xl  text-font_white"></i>
-      </Link>
-    </motion.button>
+      </button>
+    </Link>
   );
 }
