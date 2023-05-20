55738014be22950c904d93673b23df9c55bc4153
Kim Yoo <kimyoo04eco@naver.com>
2023-05-21 04:06:07 +0900

    fix: 응답 받는 user 키의 값에 따라 불필요한 코드 반복 제거

--- src/components/Rank/EducationRank/index.tsx
+++ src/components/Rank/EducationRank/index.tsx
@@ -1,6 +1,6 @@
 import { rankEducationUserData } from "public/data/rankingData";
 import EducationRankList from "./EducationRankList";
-import InterestToggle from "../DropdownItems/InterestToggle";
+import InterestDropDown from "../DropdownItems/InterestDropDown";
 
 export default function EducationRank() {
   // 더미 데이터
@@ -9,32 +9,18 @@ export default function EducationRank() {
 
   return (
     <>
-      {user && (
-        <>
-          {/* 관심사 토글 버튼 */}
-          <InterestToggle />
-          <div className="mb-8 rounded-2xl bg-main_color/5 p-4 shadow-md">
-            {/* 교육정보 최다조회 게시글 Top5 */}
-            <div className="mb-4 text-center text-lg font-medium">
-              지난주에 가장 인기 있던 교육 정보예요.
-            </div>
+      {/* //! 유저 유무에 따른 관심사 토글 버튼 */}
+      {user && <InterestDropDown />}
 
-            {/* 교육 정보 리스트 */}
-            <EducationRankList data={data}/>
-          </div>
-        </>
-      )}
-      {!user && (
-        <div className="mb-8 rounded-2xl bg-main_color/5 p-4 shadow-md">
-          {/* 교육정보 최다조회 게시글 Top5 */}
-          <div className="mb-4 text-center text-lg font-medium">
-            지난주에 가장 인기 있던 교육 정보예요.
-          </div>
+      {/* 교육정보 최다조회 게시글 Top5 */}
+      <div className="mb-8 rounded-2xl bg-main_color/5 p-4 shadow-md">
+        <h3 className="mb-4 text-center text-lg font-medium">
+          지난주에 가장 인기 있던 교육 정보예요.
+        </h3>
 
-          {/* 교육 정보 리스트 */}
-          <EducationRankList data={data}/>
-        </div>
-      )}
+        {/* 교육 정보 리스트 */}
+        <EducationRankList data={data} />
+      </div>
     </>
   );
 }
--- src/components/Rank/KewordRank/index.tsx
+++ src/components/Rank/KewordRank/index.tsx
@@ -1,5 +1,5 @@
 import PieChart from "./PieChart";
-import AgeToggle from "../DropdownItems/AgeToggle";
+import AgeDropDown from "../DropdownItems/AgeDropDown";
 import { rankEducationUserKeywords } from "public/data/rankingData";
 
 export default function KeywordRank() {
@@ -9,34 +9,17 @@ export default function KeywordRank() {
   return (
     <>
       {/* 유저 정보가 있는 경우 */}
-      {user && (
-        <>
-          {/* 연령대 토글 버튼 */}
-          <AgeToggle />
-          <div className="mb-8 rounded-2xl bg-main_color/5 p-4 shadow-md">
-            {/* 연령대별 교육정보 최다 검색 키워드 Top5 */}
-            <div className="mb-4 text-center text-lg font-medium">
-              지난주에 가장 많이 검색된 단어예요.
-            </div>
+      {user && <AgeDropDown />}
 
-            {/* 파이 차트 시각화 */}
-            <PieChart data={data.data} />
-          </div>
-        </>
-      )}
+      {/* 연령대별 교육정보 최다 검색 키워드 Top5 */}
+      <div className="mb-8 rounded-2xl bg-main_color/5 p-4 shadow-md">
+        <h3 className="mb-4 text-center text-lg font-medium">
+          지난주에 가장 많이 검색된 단어예요.
+        </h3>
 
-      {/* 유저 정보가 없는 경우 */}
-      {!user && (
-        <div className="mb-8 rounded-2xl bg-main_color/5 p-4 shadow-md">
-          {/* 교육정보 최다 검색 키워드 Top5 */}
-          <div className="mb-4 text-center text-lg font-medium">
-            지난주에 가장 많이 검색된 단어예요.
-          </div>
-
-          {/* 파이 차트 시각화 */}
-          <PieChart data={data.data} />
-        </div>
-      )}
+        {/* 파이 차트 시각화 */}
+        <PieChart data={data.data} />
+      </div>
     </>
   );
 }
