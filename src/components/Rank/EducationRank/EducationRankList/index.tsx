d4b5ed8b4e97e44000b186c08960bbef7032b86f
chae-zero <115343657+chae-zero@users.noreply.github.com>
2023-05-20 16:34:53 +0900

    feat: EducationRank 컴포넌트에 InterestToggle 추가 (유저 정보 유무 반영)

--- src/components/Rank/EducationRank/EducationRankList/index.tsx
+++ src/components/Rank/EducationRank/EducationRankList/index.tsx
@@ -1,14 +1,16 @@
-import { topEducationData } from "public/data/rankingData";
 import { IEducationData } from "@type/educations";
 import EducationRankItem from "../EducationRankItem";
+import { IRankEducationData } from "@type/rank";
 
-export default function EducationRankList() {
-  // 더미 데이터
-  const data = topEducationData;
-
+export default function EducationRankList({
+  data,
+}: {
+  data: IRankEducationData[];
+}) {
   return (
     <div className="col-center w-full gap-4">
       <ul className="grid w-full grid-cols-1">
+        {/*  교육 정보 아이템  */}
         {data.map((education) => (
           <EducationRankItem
             key={education.id}
--- src/components/Rank/EducationRank/index.tsx
+++ src/components/Rank/EducationRank/index.tsx
@@ -1,13 +1,40 @@
+import { rankEducationUserData } from "public/data/rankingData";
 import EducationRankList from "./EducationRankList";
+import InterestToggle from "../DropdownItems/InterestToggle";
 
 export default function EducationRank() {
+  // 더미 데이터
+  const data = rankEducationUserData.data;
+  const user = rankEducationUserData.user;
+
   return (
-    <div className="mb-8 rounded-2xl bg-main_color/5 p-4 shadow-md">
-      {/* 교육정보 최다조회 게시글 Top5 */}
-      <div className="mb-4 text-center text-lg font-medium">
-        지난주에 가장 인기 있던 교육 정보예요.
-      </div>
-      <EducationRankList />
-    </div>
+    <>
+      {user && (
+        <>
+          {/* 관심사 토글 버튼 */}
+          <InterestToggle />
+          <div className="mb-8 rounded-2xl bg-main_color/5 p-4 shadow-md">
+            {/* 교육정보 최다조회 게시글 Top5 */}
+            <div className="mb-4 text-center text-lg font-medium">
+              지난주에 가장 인기 있던 교육 정보예요.
+            </div>
+
+            {/* 교육 정보 리스트 */}
+            <EducationRankList data={data}/>
+          </div>
+        </>
+      )}
+      {!user && (
+        <div className="mb-8 rounded-2xl bg-main_color/5 p-4 shadow-md">
+          {/* 교육정보 최다조회 게시글 Top5 */}
+          <div className="mb-4 text-center text-lg font-medium">
+            지난주에 가장 인기 있던 교육 정보예요.
+          </div>
+
+          {/* 교육 정보 리스트 */}
+          <EducationRankList data={data}/>
+        </div>
+      )}
+    </>
   );
 }
