cedbbf6283327b3985649fce1ad23f181e748b73
Kim Yoo <kimyoo04eco@naver.com>
2023-05-22 00:22:07 +0900

    fix: DropDown 버튼의 state 수정, css 수정, 아이콘 추가

--- src/components/Rank/DropdownItems/AgeDropDown.tsx
+++ src/components/Rank/DropdownItems/AgeDropDown.tsx
@@ -2,6 +2,7 @@ import { useDispatch } from "react-redux";
 import { useAppSelector } from "@toolkit/hook";
 import { ageActions } from "@features/rank/ageSlice";
 import { ageDataArr } from "@constants/userForm/userFormData";
+import { interestActions } from "@features/rank/interestSlice";
 
 export default function AgeDropDown() {
   const dispatch = useDispatch();
@@ -11,19 +12,28 @@ export default function AgeDropDown() {
     <div className="col-center relative">
       {/* toggle button */}
       <button
-        className="w-1/2 rounded-t-md bg-main_color/90 text-font_white shadow-md"
-        onClick={() => dispatch(ageActions.toggleOpen())}
+        className="flex h-6 w-52 items-center justify-between rounded-t-md bg-main_color/90  shadow-md"
+        onClick={() => {
+          dispatch(interestActions.close());
+          dispatch(ageActions.toggleOpen());
+        }}
       >
-        {selectedageStr === "" ? "연령대별로 더보기" : selectedageStr}
+        <div className="h-9 w-9"></div>
+
+        <span className="text-font_white">
+          {selectedageStr === "" ? "연령대별 더보기" : selectedageStr}
+        </span>
+
+        <i className="ri-arrow-drop-down-fill text-4xl text-font_white "></i>
       </button>
 
       {/* Dropdown */}
       {isOpen && (
-        <ul className="col-center z-1 absolute top-6 w-1/2  rounded-b-md bg-white shadow-md">
+        <ul className="col-center z-1 absolute top-6 w-52  rounded-b-md bg-white shadow-md">
           {ageDataArr.map((ageData, indx) => (
             <li
               key={indx + ageData.age}
-              className="col-center w-40 border-b"
+              className="col-center w-52 border-b"
               onClick={() => dispatch(ageActions.selectAge({ ...ageData }))}
             >
               {ageData.ageStr}
@@ -31,7 +41,7 @@ export default function AgeDropDown() {
           ))}
 
           <li
-            className="col-center w-40 border-b"
+            className="col-center w-52"
             onClick={() => dispatch(ageActions.selectTotal())}
           >
             전체
--- src/components/Rank/DropdownItems/InterestDropDown.tsx
+++ src/components/Rank/DropdownItems/InterestDropDown.tsx
@@ -2,6 +2,7 @@ import { interests } from "@constants/userForm/userFormData";
 import { useDispatch } from "react-redux";
 import { useAppSelector } from "@toolkit/hook";
 import { interestActions } from "@features/rank/interestSlice";
+import { ageActions } from "@features/rank/ageSlice";
 
 export default function InterestDropDown() {
   const dispatch = useDispatch();
@@ -13,19 +14,28 @@ export default function InterestDropDown() {
     <div className="col-center relative">
       {/* toggle button */}
       <button
-        className="w-1/2 rounded-t-md bg-main_color/90 text-font_white shadow-md"
-        onClick={() => dispatch(interestActions.toggleOpen())}
+        className="flex h-6 w-52 items-center justify-between rounded-t-md bg-main_color/90  shadow-md"
+        onClick={() => {
+          dispatch(ageActions.close());
+          dispatch(interestActions.toggleOpen());
+        }}
       >
-        {selectedInterest === "" ? "관심사별로 더보기" : selectedInterest}
+        <div className="h-9 w-9"></div>
+
+        <span className="text-font_white">
+          {selectedInterest === "" ? "관심사별 더보기" : selectedInterest}
+        </span>
+
+        <i className="ri-arrow-drop-down-fill text-4xl text-font_white "></i>
       </button>
 
       {/* Dropdown */}
       {isOpen && (
-        <ul className="col-center absolute top-6 w-1/2 rounded-b-md bg-white shadow-md">
+        <ul className="col-center absolute top-6 w-52 rounded-b-md bg-white shadow-md">
           {interests.map((interest, indx) => (
             <li
               key={indx + interest}
-              className="col-center w-40 border-b"
+              className="col-center w-52 border-b"
               onClick={() => dispatch(interestActions.selectInterest(interest))}
             >
               {interest}
@@ -33,7 +43,7 @@ export default function InterestDropDown() {
           ))}
 
           <li
-            className="col-center w-40 border-b"
+            className="col-center w-52"
             onClick={() => dispatch(interestActions.selectTotal())}
           >
             전체
