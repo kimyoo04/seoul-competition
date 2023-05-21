7625af34623ed5704ced2c4a89a1f064c92aeab1
Kim Yoo <kimyoo04eco@naver.com>
2023-05-21 15:01:02 +0900

    fix: 시각화 컴포넌트에 dummy data 대신 커스텀 useQuery 함수 적용

--- src/components/Rank/EducationRank/index.tsx
+++ src/components/Rank/EducationRank/index.tsx
@@ -1,26 +1,34 @@
-import { rankEducationUserData } from "public/data/rankingData";
 import EducationRankList from "./EducationRankList";
 import InterestDropDown from "../DropdownItems/InterestDropDown";
+import { useReadEducationInterestRank } from "@api/rank/readEducationInterestRank";
 
 export default function EducationRank() {
-  // 더미 데이터
-  const data = rankEducationUserData.data;
-  const user = rankEducationUserData.user;
+  const { data, isLoading, isError } = useReadEducationInterestRank();
 
   return (
-    <section>
-      {/* //! 유저 유무에 따른 관심사 토글 버튼 */}
-      {user && <InterestDropDown />}
+    <>
+      {isLoading ? (
+        <div></div>
+      ) : isError ? (
+        <div>에러가 발생했습니다.</div>
+      ) : (
+        data.data.length > 0 && (
+          <section>
+            {/* //! 유저 유무에 따른 관심사 토글 버튼 */}
+            {data.user && <InterestDropDown />}
 
-      {/* 교육정보 최다조회 게시글 Top5 */}
-      <div className="mb-8 rounded-2xl bg-main_color/5 p-4 shadow-md">
-        <h3 className="mb-4 text-center text-lg font-medium">
-          지난주에 가장 인기 있던 교육 정보예요.
-        </h3>
+            {/* 교육정보 최다조회 게시글 Top5 */}
+            <div className="mb-8 rounded-2xl bg-main_color/5 p-4 shadow-md">
+              <h3 className="mb-4 text-center text-lg font-medium">
+                지난주에 가장 인기 있던 교육 정보예요.
+              </h3>
 
-        {/* 교육 정보 리스트 */}
-        <EducationRankList data={data} />
-      </div>
-    </section>
+              {/* 교육 정보 리스트 */}
+              <EducationRankList data={data.data} />
+            </div>
+          </section>
+        )
+      )}
+    </>
   );
 }
--- src/components/Rank/KewordRank/index.tsx
+++ src/components/Rank/KewordRank/index.tsx
@@ -1,25 +1,34 @@
 import PieChart from "./PieChart";
 import AgeDropDown from "../DropdownItems/AgeDropDown";
-import { rankEducationUserKeywords } from "public/data/rankingData";
+import { useReadKeywordAgeRank } from "@api/rank/readKeywordAgeRank";
 
 export default function KeywordRank() {
-  const data = rankEducationUserKeywords;
-  const user = data.user;
+  const { data, isLoading, isError } = useReadKeywordAgeRank();
 
   return (
-    <section>
-      {/* 유저 정보가 있는 경우 */}
-      {user && <AgeDropDown />}
+    <>
+      {isLoading ? (
+        <div></div>
+      ) : isError ? (
+        <div>에러가 발생했습니다.</div>
+      ) : (
+        data.data.length > 0 && (
+          <section>
+            {/* //! 유저 유무에 따른 연령대별 토글 버튼 */}
+            {data.user && <AgeDropDown />}
 
-      {/* 연령대별 교육정보 최다 검색 키워드 Top5 */}
-      <div className="mb-8 rounded-2xl bg-main_color/5 p-4 shadow-md">
-        <h3 className="mb-4 text-center text-lg font-medium">
-          지난주에 가장 많이 검색된 단어예요.
-        </h3>
+            {/* 연령대별 교육정보 최다 검색 키워드 Top5 */}
+            <div className="mb-8 rounded-2xl bg-main_color/5 p-4 shadow-md">
+              <h3 className="mb-4 text-center text-lg font-medium">
+                지난주에 가장 많이 검색된 단어예요.
+              </h3>
 
-        {/* 파이 차트 시각화 */}
-        <PieChart data={data.data} />
-      </div>
-    </section>
+              {/* 파이 차트 시각화 */}
+              <PieChart data={data.data} />
+            </div>
+          </section>
+        )
+      )}
+    </>
   );
 }
