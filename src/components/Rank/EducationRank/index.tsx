31ee07cc1e99f82a745051b35c9a557dbbc880d2
Kim Yoo <kimyoo04eco@naver.com>
2023-05-22 00:27:37 +0900

    fix: 3가지 시각화 컴포넌트의 UI/UX 개선, css 수정, state 적용

--- src/components/Rank/EducationRank/index.tsx
+++ src/components/Rank/EducationRank/index.tsx
@@ -1,43 +1,53 @@
 import EducationRankList from "./EducationRankList";
 import InterestDropDown from "../DropdownItems/InterestDropDown";
 import { useReadEducationInterestRank } from "@api/rank/readEducationInterestRank";
+import DropDown from "../DropdownItems/DropDown";
+import { useAppSelector } from "@toolkit/hook";
+import RankNotFound from "../RankNotFound";
+import RankServerError from "../RankServerError";
+import RankLoading from "../RankLoading";
 
 export default function EducationRank() {
   const { data, isLoading, isError } = useReadEducationInterestRank();
+  const interest = useAppSelector((state) => state.interest.selectedInterest);
 
   return (
-    <section>
-      {/* //! 유저 유무에 따른 연령대별 토글 버튼 */}
-      {isLoading ? (
-        <div></div>
-      ) : isError ? (
-        <div>유저 정보를 받지 못했습니다.</div>
-      ) : (
-        data.user && <InterestDropDown />
-      )}
+    <div className="w-full">
+      <h3 className="mb-4 text-center text-2xl font-bold">
+        지난주에 가장 인기 있던 교육 정보예요.
+      </h3>
 
-      {/* //! 교육정보 최다조회 게시글 Top5 */}
-      <div className="mb-8 rounded-2xl bg-main_color/5 p-4 shadow-md">
-        <h3 className="mb-4 text-center text-lg font-medium">
-          지난주에 가장 인기 있던 교육 정보예요.
-        </h3>
-
+      <section>
+        {/* //! 유저 유무에 따른 연령대별 토글 버튼 */}
         {isLoading ? (
-          <div></div>
+          <DropDown text={interest} />
         ) : isError ? (
-          <div>에러가 발생했습니다.</div>
+          <DropDown text="유저 정보를 받지 못했습니다." />
+        ) : data.user ? (
+          <InterestDropDown />
         ) : (
-          data && (
-            <>
-              {data.data.length > 0 ? (
-                <EducationRankList data={data.data} />
-              ) : (
-                <div>지난주 기록이 없습니다.</div>
-              )}
-            </>
-          )
+          <DropDown text="인기 교육 정보" />
         )}
-      </div>
-    </section>
+
+        {/* //! 교육정보 최다조회 게시글 Top5 */}
+        <div className="w-full rounded-2xl bg-main_color/5 p-4 shadow-md">
+          {isLoading ? (
+            <RankLoading />
+          ) : isError ? (
+            <RankServerError />
+          ) : (
+            data && (
+              <>
+                {data.data.length > 0 ? (
+                  <EducationRankList data={data.data} />
+                ) : (
+                  <RankNotFound />
+                )}
+              </>
+            )
+          )}
+        </div>
+      </section>
+    </div>
   );
 }
