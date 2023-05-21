8f99fd7c4ea9fe6863e302737a5dedce551e47a8
Kim Yoo <kimyoo04eco@naver.com>
2023-05-22 01:02:26 +0900

    fix: contents의 설명 문구 수정 및 도우미 챗봇 -> 가이드 챗봇

--- src/constants/home/contents.ts
+++ src/constants/home/contents.ts
@@ -2,19 +2,19 @@ export const contents = [
   {
     title: "교육 정보",
     link: "educations",
-    content: "공공데이터 API를 통한 교육 정보를 제공합니다.",
+    content: "중장년을 위한 맞춤형 교육 정보를 확인해 보세요.",
     img: "/images/open-book.png",
   },
   {
     title: "자유게시판",
     link: "posts",
-    content: "익명으로 자유롭게 교육, 취업 관련 이야기를 합니다.",
+    content: "익명으로 자유롭게 교육, 취업 관련 이야기를 해보세요.",
     img: "/images/group.png",
   },
   {
-    title: "도우미 챗봇",
+    title: "가이드 챗봇",
     link: "chatbot",
-    content: "웹사이트 사용방법과 질문에 답을 합니다.",
+    content: "웹사이트 사용 방법과 문의 사항에 대해 질문해 보세요.",
     img: "/images/chat.png",
   },
 ];
